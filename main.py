1ccaf831baa5c64d2d475d116c20d20ebbfa9acf
Thomas <thomashenderson305@gmail.com>
2023-03-07 23:46:28 +0100

    Intents

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import os
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='!')
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 for filename in os.listdir('./commands'):
     if filename.endswith('.py'):
