1407b29cddb37afbf5b23f93f16cc4307cb098ed
Thomas <thomashenderson305@gmail.com>
2023-03-09 18:21:17 +0100

    prefix

--- commands/segitseg.py
+++ commands/segitseg.py
@@ -8,11 +8,11 @@ class Segitsegcommand(commands.Cog):
 
     @commands.command()
     async def segitseg(self, ctx):
-        embed = discord.Embed(title="Ez a segítség menü. tt találod meg a parancsaimat!", description = "Minden parancs elé tegyél !-et hogy működjön a parancs!", color = 0xff0000)
+        embed = discord.Embed(title="Ez a segítség menü. tt találod meg a parancsaimat!", description = "Minden parancs elé tegyél **t!**-et hogy működjön a parancs!", color = 0xff0000)
 
         embed.timestamp = datetime.datetime.utcnow()
         embed.set_author(icon_url=ctx.author.avatar_url, name=f'{ctx.author}')
-        embed.add_field(name="!segitseg", value="Ez a menü rendszer Itt a találod a parancsaimat!", inline=False)
+        embed.add_field(name="t!segitseg", value="Ez a menü rendszer Itt a találod a parancsaimat!", inline=False)
         embed.set_footer(icon_url=ctx.guild.icon_url, text=ctx.guild.name)
         await ctx.send(embed=embed)
 
--- main.py
+++ main.py
@@ -14,6 +14,6 @@ for filename in os.listdir('./commands'):
 @bot.event
 async def on_ready():
     print('Bejelentkezve mint: {0} ({0.id})'.format(bot.user))
-    await bot.change_presence(activity=discord.Game(name="Kezdésnek írd be, hogy: !segitseg"))
+    await bot.change_presence(activity=discord.Game(name="Kezdésnek írd be, hogy: t!segitseg"))
 
 bot.run('MTA4MjMxMjk2ODUyNTU4MjQ2Nw.GSLtzn.vSylb0vNEt1Ry3LLOHyQbiULjm9IGCBLKkbfds')
